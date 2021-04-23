aa59a71308a6a52b7bd653abe7a4206291841f7a
Xinlong Chen <2815641541@qq.com>
2021-04-23 20:51:39 +0800

    add code

---
+++ cmake-build-debug/CMakeFiles/cpp_learn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_learn.dir/main.cpp.o"
+  "cpp_learn"
+  "cpp_learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
